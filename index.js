3b83f04d41dcbbe82c3c758641fc49d8217294bd
Waleed <waleedamjad99@hotmail.com>
2022-05-16 23:47:57 +0500

    Add Mobile Nav-bar

---
+++ index.js
@@ -0,0 +1,5 @@
+const menu = document.getElementById('menuButton');
+
+// var activemenu;
+activemenu= document.getElementById('menuButton').innerHTML = 'background: rgba(255, 255, 255, 0);'
+                                                               
