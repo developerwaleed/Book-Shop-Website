17bffbb833fd62268851e93b1dc3342bf5335c88
Waleed <waleedamjad99@hotmail.com>
2022-05-17 16:42:18 +0500

    Fix some errors

--- index.js
+++
@@ -1,5 +0,0 @@
-const menu = document.getElementById('menuButton');
-
-// var activemenu;
-activemenu= document.getElementById('menuButton').innerHTML = 'background: rgba(255, 255, 255, 0);'
-                                                               
